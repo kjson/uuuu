4f35fa24bd8157d3e45b795e2b6c3acbf92cf53e
Kevin Johnson <kevinstuartjohnson@gmail.com>
2017-09-28 04:40:21 -0400

    sihap

---
+++ setup.py
@@ -0,0 +1,12 @@
+''' installs u '''
+from setuptools import setup
+
+setup(
+    name='u',
+    version='0.0.1',
+    description='misc utilities.',
+    url='https://github.com/kjson/u',
+    packages=['u'],
+    zip_safe=False,
+    install_requires=[],
+)
